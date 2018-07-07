384020b7105f047d718b763f1ab0665db324ceeb
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2018-07-06 22:57:25 -0300

    Simplify Sign Extend

--- Verilogs/SignExtend/SignExtend.v
+++ Verilogs/SignExtend/SignExtend.v
@@ -2,27 +2,25 @@ module SignExtend
 #(
 	parameter DATA_WIDTH = 32
 )(
-	input [31:0] Input,
+	input [DATA_WIDTH -1:0] Input,
 	input [2:0] control,
-	output reg [31:0] outputSE
+	output reg [DATA_WIDTH -1:0] outputSE
 );
 
-	wire signed [7:0] byte1, byte0;
-	assign {byte1,byte0} = Input[15:0];
 
 	always @ ( * ) begin
 		case (control)
 			1:begin //Sign extends halfword to word
-				outputSE = ({byte1, byte0}<0)? {16'hffff, byte1, byte0} : {16'h0, byte1, byte0};
+				outputSE = (Input[15])? {16'hffff,Input[15:0]} : {16'h0, Input[15:0]};
 			end
 			2:begin //Sign extends byte to word
-				outputSE = (byte0<0)? {16'hffff, 8'hff, byte0} : {16'h0, 8'h0, byte0};
+				outputSE = (Input[7])? {24'hffffff, Input[7:0]} : {24'h0, Input[7:0]};
 			end
 			3:begin//Zero extends halfword
-				outputSE = {16'h0, byte1, byte0};
+				outputSE = {16'h0, Input[15:0]};
 			end
 			4:begin///Zero extends byte
-				outputSE = {16'h0, 8'h0, byte0};
+				outputSE = {24'h0, Input[7:0]};
 			end
 			default:begin
 				outputSE = Input;
