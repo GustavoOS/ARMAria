7a59adb7cfad6cf91a20cc70b99103f3da764172
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2019-12-14 02:37:59 -0300

    Fix SWI condition length

--- Verilogs/ControlUnit/InstructionDecoder/instructiondecoder.v
+++ Verilogs/ControlUnit/InstructionDecoder/instructiondecoder.v
@@ -320,7 +320,7 @@ always @ ( * ) begin
         ID=7'h48;
         Offset = 9; //Branches to 0x9
         RegB=4'hd;  //Link Register
-        branch_condition = 4'he; //Always branches
+        branch_condition = 5'he; //Always branches
       end
 
       13:begin//Instruction 73 - B immediate
